b9fd5852a6f827fb27e5b677c2a2701da8e4dabf
Alex Phun <alphun@ucsc.edu>
2021-07-13 21:09:10 -0700

    NewIdea

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,38 +2,41 @@ class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
         let song;
-        let click;
+        let allsound;
     }
 
     init() {
 
     }
     preload() {
-        // any asset for this scene
+        
         this.load.image('menu', './assets/MenuScreen.png');
+        this.load.audio('sfx_bgm', './assets/bgm.wav');
     }
-    create(){        
-        //this.song.play();
-        let menuConfig = {
-            fontFamily: 'Courier',
-            fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
-            align: 'right',
-            padding: {
-                top: 5, bottom: 5
-            },
-            fixedWidth: 0
-            
+    create(){    
+        
+        var allsound = this.sound.getAll();
+        if((allsound[0]) == null){
+            this.song = this.sound.add('sfx_bgm',{volume: 0.5, loop: true});
         }
+        allsound = null; 
         this.background = this.add.image(game.config.width/2, game.config.height/2, 'menu');        
         
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyH = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.H);
     }
     update() {
+        var allsound = this.sound.getAll();
+        if(allsound[0].isPlaying == false){
+            this.song.play();
+        }
+        allsound = null;
         if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
             this.scene.start('playScene');
+        }
+        if (Phaser.Input.Keyboard.JustDown(keyH)){
+            this.scene.start('helpScene');
         }
     }
 }
